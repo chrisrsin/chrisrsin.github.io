8125d8e0ca049d4a96109c991a4626d42f825b6e
chrisrsin <chrissin.2018@gmail.com>
2022-03-11 10:15:43 -0800

    Created a webpage to upload/post assignments for the course. Two assignments added - Assignment 2a, Assignment 4

---
+++ Assignment 4/fancifymytext.js
@@ -0,0 +1,27 @@
+
+
+function bigger() {
+    var text = document.getElementById("text");
+    text.style.fontSize = "4em"
+}
+
+function fancify() {
+    var text = document.getElementById("text");
+    text.style.fontWeight = "bold";
+    text.style.color = "blue";
+    text.style.textDecoration = "underline";
+}
+
+function boring() {
+    var text = document.getElementById("text");
+    text.style.fontWeight = "normal";
+    text.style.color = "black";
+    text.style.textDecoration = "none";
+}
+
+function moo() {
+    var text = document.getElementById("text");
+    text.style.textTransform = "uppercase";
+    var parts = text.value.split(".");
+    text.value = parts.join("-MOO");
+}
